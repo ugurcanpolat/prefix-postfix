5b52eed38183cb7371b125aad102a65b782b4405
Uğurcan Polat <ugurcanpolat@users.noreply.github.com>
2017-03-13 23:31:30 +0200

    Fix indentations

--- functions.h
+++ functions.h
@@ -21,57 +21,57 @@
 #define BINARY   2
 #define POLY  1000
 
-#define ABS		 0
+#define ABS	 0
 #define SQRT	 1
 
-#define SUB 	 2
-#define DIV		 3
+#define SUB	 2
+#define DIV	 3
 
-#define EXP	 	 4
-#define LOG	 	 5
-#define MOD		 6
+#define EXP	 4
+#define LOG	 5
+#define MOD	 6
 
-#define SUM		 7
+#define SUM	 7
 #define PRODUCT	 8
-#define MIN		 9
-#define MAX		10
+#define MIN	 9
+#define MAX	10
 
 struct Node {
-	char *data;
+    char *data;
     Node *next;
 };
 
 struct Queue {
-    Node *front;
-    Node *back;
-    void createQueue();
-    void closeQueue();
-    void enqueue(char*);
+    Node  *front;
+    Node  *back;
+    void  createQueue();
+    void  closeQueue();
+    void  enqueue(char*);
     char* dequeue();
 };
 
 struct Stack {
-    Node *head;
-    void createStack();
-    void closeStack();
-    void push(char*);
+    Node  *head;
+    void  createStack();
+    void  closeStack();
+    void  push(char*);
     char* pop();
 };
 
 struct Functions {
-	FILE *fp;
-	int unaryBinaryPolynary(char*);
-	int whichOperator(char*);
-	long checkOperand(char*);
-	bool isOperator(char*);
-	char* convertToPostfix(char*);
-	char* integerToString(long);
-	void initialize();
-	void evaluatePostfix(char*);
-	void openInputFile(char*);
-	void readFromInputFile();
-	void printPostfix(char*);
-	void quit();
+    FILE  *fp;
+    int   unaryBinaryPolynary(char*);
+    int   whichOperator(char*);
+    long  checkOperand(char*);
+    bool  isOperator(char*);
+    char* convertToPostfix(char*);
+    char* integerToString(long);
+    void  initialize();
+    void  evaluatePostfix(char*);
+    void  openInputFile(char*);
+    void  readFromInputFile();
+    void  printPostfix(char*);
+    void  quit();
 };
 
 #endif
